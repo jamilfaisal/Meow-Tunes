da7a2dd99943f67eb3e88b6290c4ac73afb5d8f4
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2022-10-10 19:47:00 -0400

    - Add delay for tempo mechanic - Replaces tempo UI image with a transparent image to indicate the tempo mechanic is on cooldown

--- Assets/Scripts/PlayerTempo.cs
+++ Assets/Scripts/PlayerTempo.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class PlayerTempo : MonoBehaviour
@@ -11,56 +13,84 @@ public class PlayerTempo : MonoBehaviour
         current = this;
     }
 
-    public GameObject tempoFast;
-    public GameObject tempoNormal;
-    public GameObject tempoSlow;
-
+    private const int CooldownTime = 5;
+    private bool _tempoOnCooldown;
+    public Image tempoFast;
+    public Image tempoNormal;
+    public Image tempoSlow;
     private void UpdateTempoUIImage()
     {
         switch (GameManager.current.audioTempo)
         {
             case -1:
-                tempoNormal.SetActive(false);
-                tempoFast.SetActive(false);
-                tempoSlow.SetActive(true);
+                tempoNormal.gameObject.SetActive(false);
+                tempoFast.gameObject.SetActive(false);
+                tempoSlow.gameObject.SetActive(true);
+                UpdateTempoOpacity(tempoSlow);
                 break;
             case 0:
-                tempoFast.SetActive(false);
-                tempoSlow.SetActive(false);
-                tempoNormal.SetActive(true);
+                tempoFast.gameObject.SetActive(false);
+                tempoSlow.gameObject.SetActive(false);
+                tempoNormal.gameObject.SetActive(true);
+                UpdateTempoOpacity(tempoNormal);
                 break;
             case 1:
-                tempoSlow.SetActive(false);
-                tempoNormal.SetActive(false);
-                tempoFast.SetActive(true);
+                tempoSlow.gameObject.SetActive(false);
+                tempoNormal.gameObject.SetActive(false);
+                tempoFast.gameObject.SetActive(true);
+                UpdateTempoOpacity(tempoFast);
                 break;
         }
     }
     
     public void SpeedUp(InputAction.CallbackContext context)
     {
-        
-            if (context.performed)
-            {
-                if (GameManager.current.IsGamePaused()) return;
-                ChangingTempo?.Invoke();
-                Conductor.current.IncreaseTempo();
-                GameManager.current.IncreaseAudioTempo();
-                UpdateTempoUIImage();
-            }
-        
+        if (!context.performed || GameManager.current.IsGamePaused() || _tempoOnCooldown) return;
+        TriggerTempoCooldown();
+        ChangingTempo?.Invoke();
+        Conductor.current.IncreaseTempo();
+        GameManager.current.IncreaseAudioTempo();
+        UpdateTempoUIImage();
     }
 
     public void SlowDown(InputAction.CallbackContext context)
     {
-        if (context.performed)
-            {
-                if (GameManager.current.IsGamePaused()) return;
-                ChangingTempo?.Invoke();
-                Conductor.current.DecreaseTempo();
-                GameManager.current.DecreaseAudioTempo();
-                UpdateTempoUIImage();
+        if (!context.performed || GameManager.current.IsGamePaused() || _tempoOnCooldown) return;
+        TriggerTempoCooldown();
+        ChangingTempo?.Invoke();
+        Conductor.current.DecreaseTempo();
+        GameManager.current.DecreaseAudioTempo();
+        UpdateTempoUIImage();
+    }
 
-            }
+    private void TriggerTempoCooldown()
+    {
+        _tempoOnCooldown = true;
+        StartCoroutine(DecreaseCooldownTime());
+    }
+    
+    private IEnumerator DecreaseCooldownTime()
+    {
+        var i = 0;
+        while (i < CooldownTime)
+        {
+            if (GameManager.current.IsGamePaused()) yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(1f);
+            i++;
+        }
+        _tempoOnCooldown = false;
+        UpdateTempoUIImage();
+    }
+
+    private void UpdateTempoOpacity(Graphic tempoImage)
+    {
+        ChangeTempoOpacity(tempoImage, _tempoOnCooldown ? 0.5f : 1f);
+    }
+    
+    private static void ChangeTempoOpacity(Graphic tempoImage, float opacity)
+    {
+        var temp = tempoImage.color;
+        temp.a = opacity;
+        tempoImage.color = temp;
     }
 }
