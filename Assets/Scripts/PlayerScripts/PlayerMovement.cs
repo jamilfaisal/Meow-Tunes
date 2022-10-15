73fd0c57bc0dc7b150e3dd00c6d6ebbd2974a05f
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2022-10-14 21:32:20 -0400

    Fix jump and stomp on controller (pause menu still doesnt work idk why)

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,7 +4,6 @@ using Random = UnityEngine.Random;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform cameraMainTransform;
     [SerializeField]
     private InputActionReference movement;
     [Header("Sound Effects")]
