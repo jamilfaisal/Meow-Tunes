ab2385d1d5c7619d3bed9fc8d2b8c9981a0fba02
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2022-10-14 20:45:51 -0400

    - Archive old movement script - Refactor new movement script (remove unused variables, add private modifiers, reorder multiplication to improve performance) - Update new movement script to only move left and right - Update JetBrains Rider to latest version

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
@@ -15,78 +12,66 @@ public class PlayerMovement : MonoBehaviour
     public AudioSource jumpSound2;
     public AudioSource jumpSound3;
     public AudioSource jumpSound4;
-    private AudioSource[] jumpSounds;
-    private int lastJumpSound = -1;
+    private AudioSource[] _jumpSounds;
+    private int _lastJumpSound = -1;
     
     public AudioSource landFromJumpSound;
-    private bool _justLanded = false;
+    private bool _justLanded;
 
     public AudioSource walkingSound;
     private bool _isMoving;
     
     [Header("Movement")]
-    private float moveSpeed;
+    private float _moveSpeed;
     public float walkSpeed;
-    public float sprintSpeed;
 
     public float groundDrag;
 
     public float maxJumpForce;
-    private bool jumpKeyHeld;
+    private bool _jumpKeyHeld;
     public float jumpCooldown;
     public float airMultiplier;
-    bool readyToJump;
-    bool canDoubleJump;
-    bool canSaveJump;
+    private bool _readyToJump;
+    private bool _canDoubleJump;
+    private bool _canSaveJump;
     public float stompForce = 3f;
-    public Gamepad gamepad;
     public float jumpingGravity;
-    [Header("Keybinds")]
-    public KeyCode sprintKey = KeyCode.LeftShift;
     public KeyCode stompKey = KeyCode.Tab;
 
     [Header("Ground Check")]
     public float playerHeight;
     public float playerWidth;
-    public LayerMask whatIsGround;
-    private bool grounded;
-    private RaycastHit sphereHit;
+    private bool _grounded;
 
     [Header("Slope Handling")]
     public float maxSlopeAngle;
-    private RaycastHit slopeHit;
-    private bool exitingSlope;
+    private RaycastHit _slopeHit;
+    private bool _exitingSlope;
+    
+    private float _horizontalInput;
+    private float _verticalInput;
 
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
+    private Vector3 _moveDirection;
+    private Rigidbody _rb;
 
     public MovementState state;
 
     //Tracking current player's movement
     public enum MovementState {
-        walking,
-        sprinting,
-        air
+        Walking,
+        Air
     }
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
 
-        readyToJump = true;
-        canDoubleJump = false;
-        canSaveJump = true;
+        _readyToJump = true;
+        _canDoubleJump = false;
+        _canSaveJump = true;
 
-        gamepad = Gamepad.current;
-      
-        jumpSounds = new[] { jumpSound1, jumpSound2, jumpSound3, jumpSound4 };
+        _jumpSounds = new[] { jumpSound1, jumpSound2, jumpSound3, jumpSound4 };
     }
 
 
@@ -94,29 +79,30 @@ public class PlayerMovement : MonoBehaviour
     {
         // ground check shoot a sphere to the foot of the player
         // Cast origin and the sphere must not overlap for it to work, thus we make the origin higher
-        float sphereCastRadius = playerWidth * 0.5f;
-        float sphereCastTravelDist = playerHeight * 0.5f - playerWidth * 0.5f + 0.3f;
-        grounded = Physics.SphereCast(transform.position + Vector3.up*sphereCastTravelDist*2, sphereCastRadius, Vector3.down, out sphereHit, sphereCastTravelDist*2.1f);
+        var sphereCastRadius = playerWidth * 0.5f;
+        var sphereCastTravelDist = playerHeight * 0.5f - playerWidth * 0.5f + 0.3f;
+        _grounded = Physics.SphereCast(transform.position + Vector3.up * (sphereCastTravelDist * 2),
+            sphereCastRadius, Vector3.down, out _, sphereCastTravelDist*2.1f);
         
         MyInput();
         SpeedControl();
         MovementStateHandler();
 
         // handle drag
-        if (grounded)
-            rb.drag = groundDrag;
+        if (_grounded)
+            _rb.drag = groundDrag;
         else
-            rb.drag = 0;
+            _rb.drag = 0;
 
-        if (_justLanded && grounded)
+        if (_justLanded && _grounded)
         {
             landFromJumpSound.Play();
             _justLanded = false;
-            canSaveJump = true;
+            _canSaveJump = true;
         }
         
-        if (rb.velocity.magnitude > 1 && grounded && !walkingSound.isPlaying) walkingSound.Play();
-        if (rb.velocity.magnitude <= 0 || !grounded) walkingSound.Stop();
+        if (_rb.velocity.magnitude > 1 && _grounded && !walkingSound.isPlaying) walkingSound.Play();
+        if (_rb.velocity.magnitude <= 0 || !_grounded) walkingSound.Stop();
     
     }
 
@@ -124,47 +110,44 @@ public class PlayerMovement : MonoBehaviour
     {
         MovePlayer();
 
-        if(!grounded && !jumpKeyHeld &&Vector3.Dot(rb.velocity, Vector3.up) > 0){
-            rb.AddForce(Vector3.down * 30f * rb.mass);
+        if(!_grounded && !_jumpKeyHeld &&Vector3.Dot(_rb.velocity, Vector3.up) > 0){
+            _rb.AddForce(Vector3.down * (30f * _rb.mass));
         }
     }
 
     private void MyInput()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-
         // when to jump
         if(Input.GetButtonDown("Jump")){
-            jumpKeyHeld = true;
+            _jumpKeyHeld = true;
 
-            if(readyToJump && grounded){
+            if(_readyToJump && _grounded){
 
-                readyToJump = false;
-                canDoubleJump = true;
-                canSaveJump = false;
+                _readyToJump = false;
+                _canDoubleJump = true;
+                _canSaveJump = false;
 
                 Jump();
-                pickJumpSound().Play();
+                PickJumpSound().Play();
 
                 Invoke(nameof(ResetJump), jumpCooldown);
 
             }
 
-	        if((canDoubleJump || canSaveJump) && !grounded){
+	        if((_canDoubleJump || _canSaveJump) && !_grounded){
 
-                canDoubleJump = false;
-                canSaveJump = false;
+                _canDoubleJump = false;
+                _canSaveJump = false;
 
                 HalfJump();
-                pickJumpSound().Play();
+                PickJumpSound().Play();
 
                 Invoke(nameof(ResetJump), jumpCooldown);
             }
 
         }
-        else if(Input.GetButtonUp("Jump")){
-            jumpKeyHeld = false;
+        else if (Input.GetButtonUp("Jump")){
+            _jumpKeyHeld = false;
         } else if (Input.GetKey(stompKey))
         {
             Stomp();
@@ -173,67 +156,60 @@ public class PlayerMovement : MonoBehaviour
 
     public void TriggerJump(InputAction.CallbackContext context)
     {
-        if (!context.started && this.enabled)
+        if (!context.started && enabled)
         {
-            jumpKeyHeld = true;
+            _jumpKeyHeld = true;
 
-            if(readyToJump && grounded){
+            if(_readyToJump && _grounded){
 
-                readyToJump = false;
-                canDoubleJump = true;
+                _readyToJump = false;
+                _canDoubleJump = true;
 
                 Jump();
-                pickJumpSound().Play();
+                PickJumpSound().Play();
 
                 Invoke(nameof(ResetJump), jumpCooldown);
-            } else if (context.performed && (canDoubleJump || canSaveJump) && !grounded){
+            } else if (context.performed && (_canDoubleJump || _canSaveJump) && !_grounded){
 
-                canDoubleJump = false;
-                canSaveJump = false;
+                _canDoubleJump = false;
+                _canSaveJump = false;
 
                 HalfJump();
-                pickJumpSound().Play();
+                PickJumpSound().Play();
 
                 Invoke(nameof(ResetJump), jumpCooldown);
             }
             if (context.canceled)
             {
-                jumpKeyHeld = false;
+                _jumpKeyHeld = false;
             }
         }
     }
 
-    private AudioSource pickJumpSound() {
-        int jumpSoundIndex = -1;
-        if (lastJumpSound == -1) {
+    private AudioSource PickJumpSound() {
+        int jumpSoundIndex;
+        if (_lastJumpSound == -1) {
             jumpSoundIndex = Random.Range(0, 3);
         } else {
             jumpSoundIndex = Random.Range(0, 3);
-            if (jumpSoundIndex == lastJumpSound) {
+            if (jumpSoundIndex == _lastJumpSound) {
                 jumpSoundIndex = (jumpSoundIndex + Random.Range(1, 3)) % 4;
             }
         }
-        lastJumpSound = jumpSoundIndex;
-        return jumpSounds[jumpSoundIndex];
+        _lastJumpSound = jumpSoundIndex;
+        return _jumpSounds[jumpSoundIndex];
     }
 
     private void MovementStateHandler(){
         
-        //Sprinting
-        if(grounded && (Input.GetKey(sprintKey) || gamepad != null && gamepad.rightTrigger.isPressed)){
-            state = MovementState.sprinting;
-            moveSpeed = sprintSpeed;
-        }
-
         //Walking
-        else if(grounded){
-            state = MovementState.walking;
-            moveSpeed = walkSpeed;
+        if (_grounded){
+            state = MovementState.Walking;
+            _moveSpeed = walkSpeed;
         }
-
         //in air
         else{
-            state = MovementState.air;
+            state = MovementState.Air;
         }
     }
 
@@ -241,103 +217,100 @@ public class PlayerMovement : MonoBehaviour
     {
         // calculate movement direction
 
-        Vector3 move;
-        Vector2 movementControl = movement.action.ReadValue<Vector2>();
-            move = new Vector3(movementControl.x, 0, movementControl.y);
-            move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
-            move.y = 0;
+        var movementControl = movement.action.ReadValue<Vector2>();
+        var move = new Vector3(movementControl.x, 0, 0);
+        move = cameraMainTransform.right * move.x;
+        move.y = 0;
 
-            moveDirection = move;
+        _moveDirection = move;
 
-
-            // calculate rotation (for controller movement)
-            if (movementControl != Vector2.zero || move != Vector3.zero)
-            {
-                float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
-                Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
-                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 5f);
-            }
-
-            // on slope and not jumping
-        if(OnSlope() && !exitingSlope)
+        // on slope and not jumping
+        if(OnSlope() && !_exitingSlope)
         {
-            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+            _rb.AddForce(GetSlopeMoveDirection() * (_moveSpeed * 20f), ForceMode.Force);
 
             //Prevents weird jumping motion due to no gravity on slope
-            if(rb.velocity.y > 0){
-                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            if(_rb.velocity.y > 0){
+                _rb.AddForce(Vector3.down * 80f, ForceMode.Force);
             }
         }
 
-        // on ground
-        if(grounded)
+        switch (_grounded)
         {
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+            // on ground
+            case true:
+                _rb.AddForce(_moveDirection.normalized * (_moveSpeed * 10f), ForceMode.Force);
+                break;
+            // in air
+            case false:
+                _rb.AddForce(_moveDirection.normalized * (_moveSpeed * 10f * airMultiplier), ForceMode.Force);
+                break;
         }
-
-        // in air
-        else if(!grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-            //Add some additional gravity to not make the control floaty
-            rb.AddForce(Vector3.down * jumpingGravity * rb.mass);
-
-
+        
+        //Add some additional gravity to not make the control floaty
+        _rb.AddForce(Vector3.down * (jumpingGravity * _rb.mass));
+        
         //Turn off Gravity when on slope to avoid unwanted sliding
-        rb.useGravity = !OnSlope();
+        _rb.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
     {
         // limit velocity on slope (except jumping to prevent limiting the jump)
-        if(OnSlope() && !exitingSlope){
-            if(rb.velocity.magnitude > moveSpeed)
-                rb.velocity = rb.velocity.normalized * moveSpeed;
+        if(OnSlope() && !_exitingSlope){
+            if(_rb.velocity.magnitude > _moveSpeed)
+                _rb.velocity = _rb.velocity.normalized * _moveSpeed;
         }
 
         // limit velocity on ground or air
         else{
-            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            var velocity = _rb.velocity;
+            var flatVel = new Vector3(velocity.x, 0f, velocity.z);
 
             // limit velocity if needed
-            if(flatVel.magnitude > moveSpeed)
+            if(flatVel.magnitude > _moveSpeed)
             {
-                Vector3 limitedVel = flatVel.normalized * moveSpeed;
-                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+                var limitedVel = flatVel.normalized * _moveSpeed;
+                _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z);
             }
         }
     }
 
     public void Stomp()
     {
-        if (grounded) return;
-        rb.velocity = Vector3.zero;
-        rb.AddForce(-transform.up * stompForce, ForceMode.Impulse);
+        if (_grounded) return;
+        _rb.velocity = Vector3.zero;
+        _rb.AddForce(-transform.up * stompForce, ForceMode.Impulse);
     }
 
     private void Jump()
     {
-        exitingSlope = true;
+        _exitingSlope = true;
         
         // reset y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        var velocity = _rb.velocity;
+        velocity = new Vector3(velocity.x, 0f, velocity.z);
+        _rb.velocity = velocity;
 
-        rb.AddForce(transform.up * maxJumpForce, ForceMode.Impulse);
+        _rb.AddForce(transform.up * maxJumpForce, ForceMode.Impulse);
 
     }
     private void HalfJump()
     {
-        exitingSlope = true;
+        _exitingSlope = true;
         
         // reset y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        var velocity = _rb.velocity;
+        velocity = new Vector3(velocity.x, 0f, velocity.z);
+        _rb.velocity = velocity;
 
-        rb.AddForce(transform.up * (maxJumpForce/2), ForceMode.Impulse);
+        _rb.AddForce(transform.up * (maxJumpForce/2), ForceMode.Impulse);
 
     }
     private void ResetJump()
     {
-        readyToJump = true;
-        exitingSlope = false;
+        _readyToJump = true;
+        _exitingSlope = false;
         _justLanded = true;
     }
 
@@ -347,12 +320,12 @@ public class PlayerMovement : MonoBehaviour
 
         //Increase the length of the ray a bit to make sure we are hitting the slope 
         // (may need to change based on ramp slope)
-        float RayExtension = 0.3f;
+        const float rayExtension = 0.3f;
 
         // slopeHit stores the information of the object the ray hits
-        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + RayExtension))
+        if(Physics.Raycast(transform.position, Vector3.down, out _slopeHit, playerHeight * 0.5f + rayExtension))
         {
-            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            var angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
             return angle < maxSlopeAngle && angle != 0;
         }
 
@@ -362,7 +335,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 GetSlopeMoveDirection()
     {
         //Project the move direction to the slope so that we are not moving into or away from the slope
-        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+        return Vector3.ProjectOnPlane(_moveDirection, _slopeHit.normal).normalized;
     }
 
 }
