e6029e815b48f7cfe38df8b9c3b6a447e3173081
judy-n <judynaamani@icloud.com>
2022-11-02 19:46:26 -0400

    add UI Messages

--- Assets/Scripts/ManagerScripts/ScoreManager.cs
+++ Assets/Scripts/ManagerScripts/ScoreManager.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
+
 
 public class ScoreManager : MonoBehaviour
 {
@@ -12,7 +14,10 @@ public class ScoreManager : MonoBehaviour
 
     public int playerScore;
     public TMP_Text scoreText;
-
+    public GameObject oopsText;
+    public GameObject niceText;
+    public GameObject perfectText;
+    
     private void Start()
     {
         scoreText.text = "0";
@@ -22,4 +27,36 @@ public class ScoreManager : MonoBehaviour
         playerScore += score;
         scoreText.text = playerScore.ToString();
     }
+
+    public void Hit()
+    {
+        perfectText.SetActive(true);
+        niceText.SetActive(false);
+        oopsText.SetActive(false);
+        // StartCoroutine(Delay());
+        // perfectText.SetActive(false);
+    }
+
+    public void Miss()
+    {
+        perfectText.SetActive(false);
+        niceText.SetActive(false);
+        oopsText.SetActive(true);
+        // StartCoroutine(Delay());
+        // oopsText.SetActive(false);
+    }
+
+    public void Inaccurate()
+    {
+        perfectText.SetActive(false);
+        niceText.SetActive(true);
+        oopsText.SetActive(false);
+        // StartCoroutine(Delay());
+        // niceText.SetActive(false);
+    }
+    
+    private IEnumerator Delay(){
+        yield return new WaitForSeconds(1f);
+    }
+    
 }
