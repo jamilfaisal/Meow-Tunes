2a4b7d24f6df702eb55e6d5286c54d3547ac577d
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2022-11-06 16:29:30 -0500

    - Fix platform spawning - Prespawn fish treats

--- Assets/Scripts/ManagerScripts/Lane.cs
+++ Assets/Scripts/ManagerScripts/Lane.cs
@@ -10,66 +10,33 @@ public class Lane : MonoBehaviour
     public GameObject platformPrefab;
     public GameObject fishTreatPrefab;
     public GameObject checkpointPrefab;
-    public List<Platform> platforms;
-    public List<FishHit> fishtreats;
-    private List<Tuple<int, double>> _fishTreatTimeStamps;
-
-
+    public List<Platform> platforms = new List<Platform>();
+    public List<FishHit> fishtreats = new List<FishHit>();
+    
     public float spacingSize = 2F; //based on the size of the current neutral platform
-    public double spawningHeadstartTime = 5;
 
     private const float X = 0F;
     private float _y, _z;
 
-    private int _platformSpawnIndex; //pre-spawned platforms currently does not increase spawnIndex
-    private int _fishTreatSpawnIndex;
-
-
-    public void SpawnPlatforms(IEnumerable<Note> array)
+    public void SpawnPlatformsAndFishTreats(IEnumerable<Note> array)
     {
         foreach (var note in array)
         {
             //Octave 1 is for player input
-            if (note.NoteName != platformNote || note.Octave == 1) continue;
-
+            if (note.Octave == 1) continue;
+            
             var metricTimeSpan =
                 TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, MusicPlayer.MidiFileTest.GetTempoMap());
 
             var spawnTime = ((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds +
                              (double)metricTimeSpan.Milliseconds / 1000f);
-            SpawnPlatform(note.Octave, note.Velocity, (float)spawnTime);
-        }
-    }
-    public void SetTimeStamps(IEnumerable<Note> array)
-    {
-        foreach (var note in array)
-        {
-            //Octave 1 is for player input
-            if (note.NoteName != fishTreatNote || note.Octave == 1) continue;
-            var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, MusicPlayer.MidiFileTest.GetTempoMap());
-
-            var spawnTime = ((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
-
-            if (spawnTime-spawningHeadstartTime < 0){
+            if (note.NoteName == platformNote) {
                 /* Pre-spawning platforms before game starts */
-                SpawnFishTreat(note.Octave, (float)spawnTime);
+                SpawnPlatform(note.Octave, note.Velocity, (float)spawnTime);
             }
-            else {
-                _fishTreatTimeStamps.Add(new Tuple<int, double>(note.Octave, spawnTime));
-            }
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if (Time.time < 5) return;
-        if (_fishTreatSpawnIndex < _fishTreatTimeStamps.Count)
-        {
-            if (MusicPlayer.GetAudioSourceTime() >= (_fishTreatTimeStamps[_fishTreatSpawnIndex].Item2-spawningHeadstartTime) - MusicPlayer.current.noteTime)
+            if (note.NoteName == fishTreatNote)
             {
-                /*spawning platforms based on converted time from midifile*/
-                SpawnFishTreat(_fishTreatTimeStamps[_fishTreatSpawnIndex].Item1, (float)(_fishTreatTimeStamps[_fishTreatSpawnIndex].Item2));
-                _fishTreatSpawnIndex++;
+                SpawnFishTreat(note.Octave, (float)spawnTime);
             }
         }
     }
