3fdd6217fd17dc91de7458979bbb81c4f5581a74
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2022-11-14 20:34:27 -0500

    Fix bug where checkpoint sound doesn't play by finding the checkpoint gameobject in the scene and getting its audioSource component

--- Assets/Scripts/ObjectScripts/Checkpoint.cs
+++ Assets/Scripts/ObjectScripts/Checkpoint.cs
@@ -1,9 +1,23 @@
+using System;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
     public AudioSource checkpointSound;
-    
+
+    private void Start()
+    {
+        var checkpointSoundGameObject = GameObject.FindGameObjectWithTag("checkpointSound");
+        if (checkpointSoundGameObject != null)
+        {
+            checkpointSound = checkpointSoundGameObject.GetComponent<AudioSource>();
+        }
+        else
+        {
+            throw new Exception("No gameobject with checkpointSound tag in the scene");
+        }
+    }
+
     private void OnTriggerEnter(Collider otherCollider)
     {
         if (!otherCollider.gameObject.CompareTag("Player")) return;
