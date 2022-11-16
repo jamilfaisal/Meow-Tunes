707277daccb08a2278b7ce32450ab7f5ffa35952
Daniela Venturo <daventuro@gmail.com>
2022-11-16 13:35:00 -0500

    Formatted string

--- Assets/Scripts/ManagerScripts/UIManager.cs
+++ Assets/Scripts/ManagerScripts/UIManager.cs
@@ -20,16 +20,16 @@ public class UIManager : MonoBehaviour
     public void WonLevelUI()
     {
         // Text Example: Collected 87/100 Fish Treats!
-        winLevelFishScoreText.text = "Collected " + ScoreManager.current.playerFishScore +
-                                 "/" + ScoreManager.current.maximumFishScore + " Fish Treats!";
+        winLevelFishScoreText.text = "Collected " + ScoreManager.current.playerFishScore.ToString() +
+                                 "/" + ScoreManager.current.maximumFishScore.ToString() + " Fish Treats!";
                                  
         // Text Example: Meowsic Score: 1050
-        winLevelAccuracyScoreText.text = "Meowsic Score:  " + ScoreManager.current.playerAccuracyScore;
+        winLevelAccuracyScoreText.text = "Meowsic Score:  " + ScoreManager.current.playerAccuracyScore.ToString("n0");
 
         // Text Example: Final Score: 87 x 1050 = 91350
-        winLevelFinalScoreText.text = "Final Score: " + ScoreManager.current.playerFishScore + " x " +
-                                 + ScoreManager.current.playerAccuracyScore + " = " +
-                                 (ScoreManager.current.playerFishScore*ScoreManager.current.playerAccuracyScore);
+        winLevelFinalScoreText.text = "Final Score: " + ScoreManager.current.playerFishScore.ToString() + " x " +
+                                 ScoreManager.current.playerAccuracyScore.ToString("n0") + " = " +
+                                 (ScoreManager.current.playerFishScore*ScoreManager.current.playerAccuracyScore).ToString("n0");
 
         gameUI.SetActive(false);
         winLevelUI.SetActive(true);
