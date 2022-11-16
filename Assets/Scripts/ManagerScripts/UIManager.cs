bf6d81e3765eaf54a00391607e479ed5a5cc167e
Daniela Venturo <daventuro@gmail.com>
2022-11-16 02:38:55 -0500

    Fixed win screen animation for new text

--- Assets/Scripts/ManagerScripts/UIManager.cs
+++ Assets/Scripts/ManagerScripts/UIManager.cs
@@ -24,7 +24,7 @@ public class UIManager : MonoBehaviour
                                  "/" + ScoreManager.current.maximumFishScore + " Fish Treats!";
                                  
         // Text Example: Meowsic Score: 1050
-        winLevelFishScoreText.text = "Meowsic Score:  " + ScoreManager.current.playerAccuracyScore;
+        winLevelAccuracyScoreText.text = "Meowsic Score:  " + ScoreManager.current.playerAccuracyScore;
 
         // Text Example: Final Score: 87 x 1050 = 91350
         winLevelFinalScoreText.text = "Final Score: " + ScoreManager.current.playerFishScore + " x " +
