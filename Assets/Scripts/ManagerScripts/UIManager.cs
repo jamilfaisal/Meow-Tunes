822c13ecede68968201c6b93589d62d65694073a
Daniela Venturo <daventuro@gmail.com>
2022-11-16 02:23:38 -0500

    Added text to final win screen

--- Assets/Scripts/ManagerScripts/UIManager.cs
+++ Assets/Scripts/ManagerScripts/UIManager.cs
@@ -6,7 +6,9 @@ public class UIManager : MonoBehaviour
     public static UIManager current;
 
     public GameObject winLevelUI;
-    public TMP_Text winLevelScoreText;
+    public TMP_Text winLevelFishScoreText;
+    public TMP_Text winLevelAccuracyScoreText;
+    public TMP_Text winLevelFinalScoreText;
     public GameObject lostLevelUI;
     public GameObject gameUI;
 
@@ -18,8 +20,17 @@ public class UIManager : MonoBehaviour
     public void WonLevelUI()
     {
         // Text Example: Collected 87/100 Fish Treats!
-        winLevelScoreText.text = "Collected " + ScoreManager.current.playerFishScore +
+        winLevelFishScoreText.text = "Collected " + ScoreManager.current.playerFishScore +
                                  "/" + ScoreManager.current.maximumFishScore + " Fish Treats!";
+                                 
+        // Text Example: Meowsic Score: 1050
+        winLevelFishScoreText.text = "Meowsic Score:  " + ScoreManager.current.playerAccuracyScore;
+
+        // Text Example: Final Score: 87 x 1050 = 91350
+        winLevelFinalScoreText.text = "Final Score: " + ScoreManager.current.playerFishScore + " x " +
+                                 + ScoreManager.current.playerAccuracyScore + " = " +
+                                 (ScoreManager.current.playerFishScore*ScoreManager.current.playerAccuracyScore);
+
         gameUI.SetActive(false);
         winLevelUI.SetActive(true);
     }
