f38af04adf04b4b8cfce743aab5623d562c5a510
GavinL26 <gavinuse00@gmail.com>
2022-11-23 15:20:24 -0500

    fixes merge conflict and changed blink timing a bit

--- Assets/Scripts/ObjectScripts/NeutralPlatform.cs
+++ Assets/Scripts/ObjectScripts/NeutralPlatform.cs
@@ -20,7 +20,7 @@ public class NeutralPlatform : Platform
     private IEnumerator BlinkDelay(Color blinkColor)
     {
         _material.color = blinkColor * 1.4f;
-        yield return new WaitForSeconds(_blinkWaitTime);
+        yield return new WaitForSeconds(_blinkWaitTime*2);
         _material.color = _startColor;
         yield return null;
     }
--- Assets/Scripts/UIScripts/PauseScreen.cs
+++ Assets/Scripts/UIScripts/PauseScreen.cs
@@ -43,13 +43,8 @@ public class PauseScreen : MonoBehaviour
     {
         pauseMenuUI.SetActive(false);
         settingsMenuUI.SetActive(false);
-<<<<<<< HEAD
         //MidiManager.current.ResumePlayback();
         if (Time.time > 5)
-=======
-        MidiManager.current.ResumePlayback();
-        if (Time.timeSinceLevelLoad > 5)
->>>>>>> dev
         {
             musicPlayer.Resume();
         }
