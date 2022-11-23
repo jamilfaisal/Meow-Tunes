1cbcb70242249c19b16a3bc7509367d93af97c0b
GavinL26 <gavinuse00@gmail.com>
2022-11-23 14:45:36 -0500

    removed some commeted code

--- Assets/Scripts/ManagerScripts/PlatformManager.cs
+++ Assets/Scripts/ManagerScripts/PlatformManager.cs
@@ -1,19 +1,12 @@
 using System;
 using UnityEngine;
 
-// [System.Serializable]
-// public class BlinkUnityEvent : UnityEvent<Color>
-// {
-//     public Color blinkColor;
-// }
-
 public class PlatformManager : MonoBehaviour
 {
     public static PlatformManager current;
 
     public delegate void BlinkDelegateEvent (Color blinkColorUsed);
     public BlinkDelegateEvent BlinkEvent;
-    // public event Action BlinkEvent;
     public event Action SwitchEvent;
 
     private void Awake()
